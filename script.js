95acc50969bd852f753fef95090ffd7e2cbac7af
daxtor <szabolcskovacs2012@gmail.com>
2026-09-12 19:19:04 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,38 @@
+function inditas() {
+
+    const nev = document.querySelector("input").value;
+
+    const orszag =
+        document.querySelector("select").value;
+
+    if (nev === "") {
+
+        alert("❗ Kérlek, írd be a neved!");
+
+        return;
+    }
+
+    if (
+        orszag === "" ||
+        orszag === "-- Válassz országot --"
+    ) {
+
+        alert("❗ Kérlek, válaszd ki az országodat!");
+
+        return;
+    }
+
+    localStorage.setItem(
+        "jatekosNev",
+        nev
+    );
+
+    localStorage.setItem(
+        "jatekosOrszag",
+        orszag
+    );
+
+    window.location.href =
+        "kategoria.html";
+}
 
