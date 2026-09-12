797f4fa1286b0c3eb9edbed49c43d4825ae13ebd
daxtor <szabolcskovacs2012@gmail.com>
2026-09-12 19:35:15 +0200

    Update eredmeny.js

--- eredmeny.js
+++ eredmeny.js
@@ -1,1 +1,53 @@
+const nev =
+    localStorage.getItem(
+        "jatekosNev"
+    );
+
+const orszag =
+    localStorage.getItem(
+        "jatekosOrszag"
+    );
+
+const pont =
+    localStorage.getItem(
+        "jatekosPont"
+    );
+
+
+document.getElementById(
+    "jatekosNev"
+).textContent =
+    nev || "Ismeretlen játékos";
+
+
+document.getElementById(
+    "jatekosOrszag"
+).textContent =
+    orszag || "Ismeretlen ország";
+
+
+document.getElementById(
+    "vegsoPont"
+).textContent =
+    pont || "0";
+
+
+document.getElementById(
+    "koszontes"
+).textContent =
+    `Szép munka, ${nev}! 🎉`;
+
+
+function ujJatek() {
+
+    window.location.href =
+        "kategoria.html";
+}
+
+
+function ranglista() {
+
+    window.location.href =
+        "ranglista.html";
+}
 
